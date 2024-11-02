e909736aa4b92c51c62feb5da94a56639a30a05b
bruno <camposandrew950@gmail.com>
2024-11-02 17:33:25 -0300

    correção de linhas substituidas para acrescentadas

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('form-atividade');
+let linhas = '';
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
@@ -6,13 +8,17 @@ form.addEventListener('submit', function(e){
     const inputTelefone = document.getElementById('numero-telefone');
     const inputEmail = document.getElementById('email-usuario');
 
+    
+
     let linha = '<tr>';
     linha += `<td>${inputNomeUsuario.value}</td>`;
     linha += `<td>${inputTelefone.value}</td>`;
     linha += `<td>${inputEmail.value}</td>`;
     linha += '</tr>';
 
+    linhas += linha;
+
     const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linha;
+    corpoTabela.innerHTML = linhas;
 
 })
